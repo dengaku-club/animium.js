52ec499b0f181fcc790f50fc00e0ed723a184217
Daisuke Akatsuka <dakatsuka@mozilla.com>
2017-08-05 00:31:44 +0900

    setup aspace

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const post = require('./aspace.js/src/post');
+const get = require('./aspace.js/src/get');
+
+app.use('/aspace', post);
+app.use('/aspace', get);
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});

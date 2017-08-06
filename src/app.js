8add81a7ecb98a6ad005da1c33e106dec339708d
Daisuke Akatsuka <dakatsuka@mozilla.com>
2017-08-06 21:59:55 +0900

    setup anitize

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const get = require('./aspace.js/src/get');
 
 app.use('/aspace', post);
 app.use('/aspace', get);
+app.use('/anitize', express.static(`${__dirname}/anitize.js/dist`));
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');

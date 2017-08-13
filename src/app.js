e6155ab527198c5a2ab3907186c900826a9b880e
Daisuke Akatsuka <dakatsuka@mozilla.com>
2017-08-13 18:08:28 +0900

    add a page to show animatoin list

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const get = require('./aspace.js/src/get');
 app.use('/aspace', post);
 app.use('/aspace', get);
 app.use('/anitize', express.static(`${__dirname}/anitize.js/dist`));
+app.use('/uploads', express.static(`${__dirname}/../uploads`));
+app.use('/manage', express.static(`${__dirname}/manage`));
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
